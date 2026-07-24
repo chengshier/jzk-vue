cd78dd6968925607f9afcb6fc3c93d766d90fba5
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-24 11:48:03 +0800

    feat: 增加推广素材配置API

--- src/api/jkOptions.js
+++ src/api/jkOptions.js
@@ -3,3 +3,11 @@ import request from '@/utils/request'
 export function getJkStockAccountOptions(params) {
   return request({ url: '/admin/jk/options/stock-accounts', method: 'get', params })
 }
+
+export function getJkPromotionMaterialList() {
+  return request({ url: '/admin/jk/promotion/material/list', method: 'get' })
+}
+
+export function saveJkPromotionMaterials(data) {
+  return request({ url: '/admin/jk/promotion/material/save', method: 'post', data })
+}
